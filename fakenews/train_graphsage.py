1d160e24878695ceed0f4b23595f4103e032e726
Nikos Kanakaris <nikos.kanakaris89@gmail.com>
2021-05-13 12:26:45 +0300

    Infer user embeddings

--- fakenews/train_graphsage.py
+++ fakenews/train_graphsage.py
@@ -6,19 +6,22 @@
 
 import argparse
 from stellargraph import StellarGraph
-import numpy as np
 from stellargraph.mapper import GraphSAGENodeGenerator
 from stellargraph.layer import GraphSAGE
 from tensorflow.keras import layers, Model, optimizers
 from sklearn import preprocessing, model_selection
 import utils
-import pandas as pd
-
-from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
-import sklearn
 import tensorflow
 
+
+def infer(embedding_model, vertices_df, edges_df, batch_size, num_samples):
+    hold_out_graph = StellarGraph(vertices_df, edges_df, edge_type_default='follows', node_type_default='user')
+    hold_out_gen = GraphSAGENodeGenerator(hold_out_graph, batch_size, num_samples)
+    hold_out_gen = hold_out_gen.flow(vertices_df.index)
+    if type(embedding_model) in [str]:
+        embedding_model = tensorflow.keras.models.load_model(embedding_model)
+    emb = embedding_model.predict(hold_out_gen)
+    return emb
 
 def run(args):
     edges_df =  utils.read_pickle_from_file('edges.pkl')
@@ -29,6 +32,10 @@ def run(args):
     print('###### Total labels value counts')
     print(labels.value_counts())
     labels_sampled = labels.sample(frac=0.8, replace=False, random_state=100)
+    vertices_df_sampled = vertices_df.sample(frac=0.8, replace=False, random_state=100)
+    edges_df_sampled = edges_df.sample(frac=0.8, replace=False, random_state=100)
+    edges_df_sampled = edges_df_sampled[edges_df_sampled['source'].isin(vertices_df_sampled.index.tolist())]
+    edges_df_sampled = edges_df_sampled[edges_df_sampled['target'].isin(vertices_df_sampled.index.tolist())]
 
     print('###### Labels sampled value counts')
     print(labels_sampled.value_counts())
@@ -60,9 +67,10 @@ def run(args):
     test_targets = target_encoding.transform(test_labels)
 
     print('###### Create StellarGraph graph')
-    graph_full = StellarGraph(vertices_df, edges_df, edge_type_default='follows', node_type_default='user')
-    print(graph_full.info())
-    graph_sampled = graph_full.subgraph(list(labels_sampled.index))
+    print(edges_df_sampled)
+    print(vertices_df_sampled)
+    graph_sampled = StellarGraph(vertices_df_sampled, edges_df_sampled, edge_type_default='follows', node_type_default='user')
+    print(graph_sampled.info())
 
     batch_size = 100
     num_samples = [20, 10]
@@ -70,7 +78,7 @@ def run(args):
     train_gen = generator.flow(list(train_labels.index), train_targets, shuffle=True)
 
     graphsage_model = GraphSAGE(
-        layer_sizes=[32, 20], generator=generator, bias=True, dropout=0.5, activations=['relu', 'relu']
+        layer_sizes=[32, 15], generator=generator, bias=True, dropout=0.5, activations=['relu', 'relu']
     )
     x_inp, x_out = graphsage_model.in_out_tensors()
     if train_targets.shape[1] == 1:
@@ -94,7 +102,7 @@ def run(args):
     )
     val_gen = generator.flow(val_labels.index, val_targets)
     history = model.fit(
-        train_gen, epochs=2, validation_data=val_gen, verbose=1, shuffle=False
+        train_gen, epochs=1, validation_data=val_gen, verbose=1, shuffle=False
     )
 
     test_gen = generator.flow(test_labels.index, test_targets)
@@ -102,69 +110,18 @@ def run(args):
     for name, val in zip(model.metrics_names, test_metrics):
         print("\t{}: {:0.4f}".format(name, val))
 
-    #
-    # Making predictions with the model
-    #
-    generator = GraphSAGENodeGenerator(graph_full, batch_size, num_samples)
-    hold_out_nodes = labels.index.difference(labels_sampled.index)
+    hold_out_vertices_df = vertices_df[~vertices_df.index.isin(vertices_df_sampled.index)]
+    hold_out_edges_df = edges_df[edges_df['source'].isin(hold_out_vertices_df.index.tolist())]
+    hold_out_edges_df = hold_out_edges_df[hold_out_edges_df['target'].isin(hold_out_vertices_df.index.tolist())]
 
-    labels_hold_out = labels[hold_out_nodes]
-    print('###### labels hold out value counts')
-    print(labels_hold_out.value_counts())
-    hold_out_targets = target_encoding.transform(labels_hold_out)
-    hold_out_gen = generator.flow(hold_out_nodes, hold_out_targets)
-    hold_out_predictions = model.predict(hold_out_gen)
-    hold_out_predictions = target_encoding.inverse_transform(hold_out_predictions)
-    print(len(hold_out_predictions))
-
-    results = pd.Series(hold_out_predictions, index=hold_out_nodes)
-    df = pd.DataFrame({"Predicted": results, "True": labels_hold_out})
-    print(df.head(10))
-    print('###### Precision')
-    print(sklearn.metrics.precision_score(labels_hold_out, results))
-    print('###### Recall')
-    print(sklearn.metrics.recall_score(labels_hold_out, results))
-    hold_out_metrics = model.evaluate(hold_out_gen)
-    print("\nHold Out Set Metrics:")
-    for name, val in zip(model.metrics_names, hold_out_metrics):
-        print("\t{}: {:0.4f}".format(name, val))
-
-    #
-    # Node embeddings for hold out nodes
-    #
+    utils.write_object_to_pickle_file('hold_out_vertices_df.pkl', hold_out_vertices_df)
+    utils.write_object_to_pickle_file('hold_out_edges_df.pkl', hold_out_edges_df)
+    embedding_model_filename = 'users_embedding_%s_%s_model' % (batch_size, '_'.join([str(i) for i in num_samples]))
+    print('Saving embedding_model: %s' % (embedding_model_filename))
     embedding_model = Model(inputs=x_inp, outputs=x_out)
-    emb = embedding_model.predict(hold_out_gen)
-    print('###### embedding shape')
-    print(emb.shape)
-    X = emb
-    y = np.argmax(target_encoding.transform(labels_hold_out), axis=1)
-    if X.shape[1] > 2:
-        transform = TSNE  # PCA
-        trans = transform(n_components=2)
-        emb_transformed = pd.DataFrame(trans.fit_transform(X), index=hold_out_nodes)
-        emb_transformed["label"] = y
-    else:
-        emb_transformed = pd.DataFrame(X, index=hold_out_nodes)
-        emb_transformed = emb_transformed.rename(columns={"0": 0, "1": 1})
-        emb_transformed["label"] = y
+    embedding_model.save(embedding_model_filename)
 
-    alpha = 0.7
-
-    fig, ax = plt.subplots(figsize=(10, 8))
-    ax.scatter(
-        emb_transformed[0],
-        emb_transformed[1],
-        c=emb_transformed["label"].astype("category"),
-        cmap="jet",
-        alpha=alpha,
-    )
-    ax.set(aspect="equal", xlabel="$X_1$", ylabel="$X_2$")
-    plt.title(
-        "{} visualization of GraphSAGE embeddings of hold out nodes".format(
-            transform.__name__
-        )
-    )
-    plt.show()
+    print(infer(embedding_model, hold_out_vertices_df, hold_out_edges_df, batch_size, num_samples))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
